f3cba15e55d2732546b0fb0f75b062af592c6617
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-06-03 11:10:41 -0700

    It will add a for loop in the javascript file that will loop through the questions and tally a score.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,18 @@ $(document).ready(function() {
     var tally = 0;
     var finished = true;
 
-    
+    for(var i=1; i<7; i++ ){
+      var num= i.toString();
+      var check = parseInt($("input:checkbox[name="+num+"]:checked").val());
+      if(check){
+        tally += check;
+      }else{
+        finished = false;
+      }
+    }
+    alert(tally);
+
+
 
 
   });
